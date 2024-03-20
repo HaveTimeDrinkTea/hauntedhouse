fdd45cff97b421fe816af1a85c44363f49d30430
omml <omml@hotmail.com>
2024-03-20 13:49:42 -0600

    Added passages for rat
    Added passages for rat so it can move around easier than player

--- Layout.cpp
+++ Layout.cpp
@@ -22,38 +22,74 @@ Layout::Layout()
 	corr1->setLinkedRoom(East, kitpass);
 	corr1->setLinkedRoom(South, porch);
 	corr1->setLinkedRoom(West, dinn);
+	//For rat
+	corr1->setSmallPassage(North, hall);
+	corr1->setSmallPassage(East, kitpass);
+	corr1->setSmallPassage(West, dinn);
 
 	dinn->setLinkedRoom(East, corr1);
+	//for rat
+	dinn->setSmallPassage(North, study);
+	dinn->setSmallPassage(East, corr1);
 	
 	hall->setLinkedRoom(North, corr2);
 	hall->setLinkedRoom(South, corr1);
 	hall->setLinkedRoom(West, study);
+	//for rat
+	hall->setSmallPassage(North, corr2);
+	hall->setSmallPassage(East, couyar);
+	hall->setSmallPassage(South, corr1);
+	hall->setSmallPassage(West, study);
 
 	study->setLinkedRoom(East, hall);
+	//for rat
+	study->setSmallPassage(North, draw);
+	study->setSmallPassage(East, hall);
+	study->setSmallPassage(South, dinn);
 	
 	corr2->setLinkedRoom(North, pantry1);
 	corr2->setLinkedRoom(East, pantry2);
 	corr2->setLinkedRoom(South, hall);
 	corr2->setLinkedRoom(West, draw);
+	//for rat
+	corr2->setSmallPassage(North, pantry1);
+	corr2->setSmallPassage(East, pantry2);
+	corr2->setSmallPassage(South, hall);
+	corr2->setSmallPassage(West, draw);
 	
 	draw->setLinkedRoom(East, corr2);
+	//for rat
+	draw->setSmallPassage(East, corr2);
+	draw->setSmallPassage(South, study);
 	
 	pantry1->setLinkedRoom(South, corr2);
+	//for rat
+	pantry1->setSmallPassage(South, corr2);
 	
 	pantry2->setLinkedRoom(West, corr2);
+	//for rat
+	pantry2->setSmallPassage(West, corr2);
 
 	kitpass->setLinkedRoom(North, couyar);
 	kitpass->setLinkedRoom(East, kitchen);
 	kitpass->setLinkedRoom(West, corr1);
+	//for rat
+	kitpass->setSmallPassage(North, couyar);
+	kitpass->setSmallPassage(East, kitchen);
+	kitpass->setSmallPassage(West, corr1);
 
 	couyar->setLinkedRoom( South, kitpass);
+	//for rat
+	couyar->setSmallPassage(South, kitpass);
+	couyar->setSmallPassage(West, hall);
 	
 	kitchen->setLinkedRoom( West, kitpass);
+	//for rat
+	kitchen->setSmallPassage(West, kitpass);
 
 	//TO DO: add items to the rooms
 	//TO DO: store the references to the items
 
-	
 	_roomList.push_back(porch);
 	_roomList.push_back(corr1);
 	_roomList.push_back(dinn);
